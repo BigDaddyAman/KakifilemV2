77321aa82ecf771e299a1f77d2c6a726b09f0a9b
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-03-13 08:01:50 +0800

    Update urls.py

--- videoapp/urls.py
+++ videoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('countdown/', views.countdown, name='countdown'),
+    path('health/', views.health_check, name='health_check'),  # Add this line
 ]
